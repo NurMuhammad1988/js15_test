03b2345dbafa4763b45ffb52be0e29ffd2b4a9fd
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-04-08 02:53:01 +0500

    ddddddeeee

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log ('salom');
+
+//priminitiv malumot turlariga boolean, number, string, undefind, null, symbol, bight lar kiradi
+
+//non- priminitiv malumot turlariga array, object lar kiradi    
